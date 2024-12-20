2d34247f010d025b65c3cddf2eab2e143fb3982c
Ева <EwaDrug@yandex.ru>
2024-12-20 13:07:44 +0300

    20.12.24

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Ева'
+title = 'Практика'
+content = 'Сделать задание 1'
+status = 'Активно'
+created_date = '20-12-2014'
+issue_date = '25-12-2024'
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', created_date)
+print('Дата истечения заметки:', issue_date)
